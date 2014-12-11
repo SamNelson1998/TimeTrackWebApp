2ee47b2954b9fd14b40c834332ba49bf5335aa76
Sam Nelson <snelson@elevenwinds.com>
2014-12-11 10:55:40 -0500

    commits

---
+++ TimeTracker/Controllers/TimeDataController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimeTracker.Controllers
+{
+    public class TimeDataController : Controller
+    {
+        //
+        // GET: /TimeData/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
